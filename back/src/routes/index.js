5d406cbb0c381dad14359639df2daea704ca963e
Juan Gustin <juandgustin@gmail.com>
2023-03-29 22:30:18 -0500

    Prueba

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -3,7 +3,7 @@ const router = Router();
 const getCharDetail = require('../controllers/getCharDetail');
 
 //Aqui anejamos la url y ejecutamos el controller correspondiente.
-
+//Hola
 //Nos encargamos de gestionar las peticiones de que hacen los controllers.
 //Cuando la url sea detail/:id, se ejecuta la función siguiente.
 
